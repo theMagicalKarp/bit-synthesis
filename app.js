d1205cf96af6f9bed1001f74e41925764769b6df
Robert Sheehy <gameboy1092@gmail.com>
2016-01-19 19:21:59 -0600

    Day 2: Start work in async worker com
    This also involved some looking into how the physics
    engine will interact with the rendering engine.

--- app.js
+++ app.js
@@ -1,53 +1,64 @@
-importScripts('lib/lodash.core.min.js', 'lib/cannon.min.js');
+importScripts('lib/lodash.min.js', 'lib/cannon.min.js');
 
-self.addEventListener('message', function(e) {
-  self.postMessage(e.data);
-}, false);
-
-
-var requestAnimationFrame = function(callback, element) {
-    var currTime = new Date().getTime();
-    var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-    var id = setTimeout(function() { callback(currTime + timeToCall); }, 
-      timeToCall);
-    lastTime = currTime + timeToCall;
-    return id;
-};
-
-var world = new CANNON.World();
-world.gravity.set(0, 0, -9.82); // m/s²
-
-// Create a sphere
-var radius = 1; // m
-var sphereBody = new CANNON.Body({
-   mass: 5, // kg
-   position: new CANNON.Vec3(0, 0, 10), // m
-   shape: new CANNON.Sphere(radius)
-});
-world.addBody(sphereBody);
-
-// Create a plane
-var groundBody = new CANNON.Body({
-    mass: 0 // mass == 0 makes the body static
-});
-var groundShape = new CANNON.Plane();
-groundBody.addShape(groundShape);
-world.addBody(groundBody);
-
+var world = null
+var particles = {};
+var lastTime;
 var fixedTimeStep = 1.0 / 60.0; // seconds
 var maxSubSteps = 3;
 
-// Start the simulation loop
-var lastTime;
 
-(function simloop(time){
-  requestAnimationFrame(simloop);
-  if(lastTime !== undefined){
-     var dt = (time - lastTime) / 1000;
-     world.step(fixedTimeStep, dt, maxSubSteps);
-  }
+var newWorld = function (data) {
+    world = new CANNON.World();
+    world.gravity.set(0, 0, -9.82);
 
-  self.postMessage(sphereBody.position);
-  lastTime = time;
-})();
+
+    _.range(data.count).forEach(function(i) {
+        var sphereBody = new CANNON.Body({
+            mass: 5,
+            position: new CANNON.Vec3(_.random(-3, 3, true), _.random(-3, 3, true), 10),
+            shape: new CANNON.Box(new CANNON.Vec3(1,1,1))
+        });
+
+        world.addBody(sphereBody);
+
+        particles[_.uniqueId('particle')] = sphereBody;
+    });
+
+    var groundBody = new CANNON.Body({
+        mass: 0 // mass == 0 makes the body static
+    });
+
+    var groundShape = new CANNON.Plane();
+    groundBody.addShape(groundShape);
+    world.addBody(groundBody);
+
+};
+
+self.addEventListener('message', function(e) {
+    var data = e.data;
+
+    if (data.command === 'new') {
+        newWorld(data);
+    }
+});
+
+
+setInterval(function() {
+    if (world === null) {
+        return;
+    }
+
+    var currTime = new Date().getTime();
+
+    var dt = (currTime - lastTime) / 1000;
+    world.step(fixedTimeStep, dt, maxSubSteps);
+    lastTime = currTime;
+
+
+    var data = _.keys(particles).map(function(key) {
+        return particles[key].position;
+    });
+    self.postMessage(data);
+
+}, 16);
 
