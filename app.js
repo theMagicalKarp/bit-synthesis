56444c1cc3e80597d31676b5a308feaddb26d0af
Robert Sheehy <gameboy1092@gmail.com>
2016-01-21 22:15:50 -0600

    Day 4: Start learning constraints
    Today my main focus was learning on how to
    utilize constraints.  After some experimenting I
    was able to successfully make a chain of particles. ^_^

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ importScripts('lib/lodash.min.js', 'lib/cannon.min.js');
 
 var world = null
 var particles = {};
+var constraints = [];
 var lastTime;
 var fixedTimeStep = 1.0 / 60.0;
 var maxSubSteps = 3;
@@ -17,15 +18,48 @@ var newWorld = function (data) {
             mass: 5,
             position: new CANNON.Vec3(_.random(-25, 25, true),_.random(-25, 25, true), _.random(-25, 25, true)),
             shape: new CANNON.Sphere(1),
-            velocity: new CANNON.Vec3(_.random(-1, 1, true),_.random(-1, 1, true), _.random(-1, 1, true))
+            velocity: new CANNON.Vec3(_.random(-10, 10, true),_.random(-10, 10, true), _.random(-10, 10, true))
         });
         sphereBody.angularVelocity.set(_.random(-1, 1, true),_.random(-1, 1, true),_.random(-1, 1, true));
-        // sphereBody.color = new CANNON.Vec3(_.random(.85, .95, true), _.random(.15, .30, true), _.random(.15, .30, true));
-        // console.log(sphereBody);
         world.addBody(sphereBody);
-
         particles[_.uniqueId('particle')] = sphereBody;
     });
+    var temp = _.values(particles);
+
+    for(var i =0; i <data.count-1; i++) {
+        var constraint = new CANNON.DistanceConstraint(temp[i], temp[i+1])
+        constraints.push(constraint);
+        world.addConstraint(constraint);
+    }
+    // for(var i = 0; i < data.count; i+=2) {
+    //     var constraint = new CANNON.DistanceConstraint(temp[i], temp[i+1])
+    //     constraints.push(constraint);
+    //     world.addConstraint(constraint);
+    // }
+
+
+    // var abod = new CANNON.Body({
+    //     mass: 5,
+    //     position: new CANNON.Vec3(5, 0, 0),
+    //     shape: new CANNON.Sphere(1),
+    //     velocity: new CANNON.Vec3(1,0, 0)
+    // });
+    // world.addBody(abod);
+    // var bbod = new CANNON.Body({
+    //     mass: 5,
+    //     position: new CANNON.Vec3(0, 0, 0),
+    //     shape: new CANNON.Sphere(1),
+    //     velocity: new CANNON.Vec3(-1, 5, 0)
+    // });
+    // world.addBody(bbod);
+    // particles['a'] = abod;
+    // particles['b'] = bbod;
+    // constraint = new CANNON.DistanceConstraint(abod, bbod);
+    // world.addConstraint(constraint);
+    // constraints.push(constraint);
+
+    
+
 };
 
 self.addEventListener('message', function(e) {
@@ -48,14 +82,21 @@ setInterval(function() {
     world.step(fixedTimeStep, dt, maxSubSteps);
     lastTime = currTime;
 
-    var data = _.keys(particles).map(function(key) {
+    var partData = _.keys(particles).map(function(key) {
         return {
             position: particles[key].position,
             quaternion: particles[key].quaternion,
             color: particles[key].color
         };
     });
-    self.postMessage(data);
+
+    var constData = _.map(constraints, function(constraint) {
+        return [constraint.bodyA.position, constraint.bodyB.position];
+    })
+    self.postMessage({
+        particles: partData,
+        constraints: constData
+    });
 
 }, 16);
 
