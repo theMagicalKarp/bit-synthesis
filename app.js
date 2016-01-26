ebb35675a88588f509cde793bffa1c452abdeae4
Robert Sheehy <gameboy1092@gmail.com>
2016-01-25 22:10:12 -0600

    Day 5: Refactor & FPS meter & Infection
    At the start of the coding session I revised and refactored
    some of the previous work I had done.  I also implemented
    a fps counter for both the rendering & physics engine to get
    quantitate information on performance.  I also started to play
    around with how particles will infect other particles.

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 importScripts('lib/lodash.min.js', 'lib/cannon.min.js');
 
-
-var world = null
-var particles = {};
+var i,j,k;
+var world = null;
+var infected = [];
+var particles = [];
 var constraints = [];
 var lastTime;
 var fixedTimeStep = 1.0 / 60.0;
@@ -12,9 +13,10 @@ var maxSubSteps = 3;
 var newWorld = function (data) {
     world = new CANNON.World();
     world.gravity.set(0, 0, 0);
+    var sphereBody;
 
-    _.range(data.count).forEach(function(i) {
-        var sphereBody = new CANNON.Body({
+    for (i=0; i < data.count; i++) {
+        sphereBody = new CANNON.Body({
             mass: 5,
             position: new CANNON.Vec3(_.random(-25, 25, true),_.random(-25, 25, true), _.random(-25, 25, true)),
             shape: new CANNON.Sphere(1),
@@ -22,43 +24,32 @@ var newWorld = function (data) {
         });
         sphereBody.angularVelocity.set(_.random(-1, 1, true),_.random(-1, 1, true),_.random(-1, 1, true));
         world.addBody(sphereBody);
-        particles[_.uniqueId('particle')] = sphereBody;
-    });
-    var temp = _.values(particles);
-
-    for(var i =0; i <data.count-1; i++) {
-        var constraint = new CANNON.DistanceConstraint(temp[i], temp[i+1])
-        constraints.push(constraint);
-        world.addConstraint(constraint);
+        sphereBody.color = new CANNON.Vec3(0,1,0);
+        particles.push(sphereBody);
     }
-    // for(var i = 0; i < data.count; i+=2) {
-    //     var constraint = new CANNON.DistanceConstraint(temp[i], temp[i+1])
+
+
+    sphereBody = new CANNON.Body({
+        mass: 5,
+        position: new CANNON.Vec3(0, 0, 0),
+        shape: new CANNON.Sphere(1),
+        velocity: new CANNON.Vec3(_.random(-10, 10, true),_.random(-10, 10, true), _.random(-10, 10, true))
+    });
+    sphereBody.angularVelocity.set(_.random(-1, 1, true),_.random(-1, 1, true),_.random(-1, 1, true));
+    sphereBody.color = new CANNON.Vec3(1,0,0);
+    // sphereBody.relatives = [];
+    world.addBody(sphereBody);
+    infected.push(sphereBody);
+
+
+
+
+
+    // for(i =0; i < data.count-1; i++) {
+    //     var constraint = new CANNON.DistanceConstraint(particles[i], particles[i+1])
     //     constraints.push(constraint);
     //     world.addConstraint(constraint);
     // }
-
-
-    // var abod = new CANNON.Body({
-    //     mass: 5,
-    //     position: new CANNON.Vec3(5, 0, 0),
-    //     shape: new CANNON.Sphere(1),
-    //     velocity: new CANNON.Vec3(1,0, 0)
-    // });
-    // world.addBody(abod);
-    // var bbod = new CANNON.Body({
-    //     mass: 5,
-    //     position: new CANNON.Vec3(0, 0, 0),
-    //     shape: new CANNON.Sphere(1),
-    //     velocity: new CANNON.Vec3(-1, 5, 0)
-    // });
-    // world.addBody(bbod);
-    // particles['a'] = abod;
-    // particles['b'] = bbod;
-    // constraint = new CANNON.DistanceConstraint(abod, bbod);
-    // world.addConstraint(constraint);
-    // constraints.push(constraint);
-
-    
 
 };
 
@@ -71,8 +62,10 @@ self.addEventListener('message', function(e) {
 });
 
 
-setInterval(function() {
+var tick = function() {
+
     if (world === null) {
+        setTimeout(tick, 16);
         return;
     }
 
@@ -82,21 +75,55 @@ setInterval(function() {
     world.step(fixedTimeStep, dt, maxSubSteps);
     lastTime = currTime;
 
-    var partData = _.keys(particles).map(function(key) {
-        return {
-            position: particles[key].position,
-            quaternion: particles[key].quaternion,
-            color: particles[key].color
-        };
-    });
+    var stuff = []
+    for (j=0; j<infected.length; j++) {
+        for (i=0; i<particles.length; i++) {
+            if (infected[j].position.distanceTo(particles[i].position) < 10) {
+                stuff.push(particles[i]);
+                particles[i].color.x = 1;
+                particles[i].color.y = 0;
+                var constraint = new CANNON.DistanceConstraint(infected[j], particles[i]);
+                constraints.push(constraint);
+                world.addConstraint(constraint);
+            }
+        }
+    }
 
-    var constData = _.map(constraints, function(constraint) {
-        return [constraint.bodyA.position, constraint.bodyB.position];
-    })
+    for (j=0; j<stuff.length; j++) {
+        particles.splice(particles.indexOf(stuff[j]), 1);
+        infected.push(stuff[j])
+    }
+
+
+
+
+    var partData = [];
+    for (i=0; i < particles.length; i++) {
+        partData.push({
+            position: particles[i].position,
+            quaternion: particles[i].quaternion,
+            color: particles[i].color
+        });
+    }
+
+    for (i=0; i < infected.length; i++) {
+        partData.push({
+            position: infected[i].position,
+            quaternion: infected[i].quaternion,
+            color: infected[i].color
+        });
+    }
+
+
+    var constData = [];
+    for (i=0; i < constraints.length; i++) {
+        constData.push([constraints[i].bodyA.position, constraints[i].bodyB.position]);
+    }
+
     self.postMessage({
         particles: partData,
         constraints: constData
     });
-
-}, 16);
-
+    setTimeout(tick, 8);
+};
+tick();
